387528eced3f649eb500dee9554e9465235716e9
Thomas Sweeney <magneticmule@gmail.com>
2015-11-25 03:38:22 +0000

    Added feedparser

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var Feedparser = require('feedparser'),
+    request = require('request');
+
+var req = request(),
+    feedparser = new Feedparser({});
+
+req.on('error', function (error) {
+    console.log("Error: " + error.toString());
+});
+
+req.on('response', function (res) {
+    var newsStream = this;
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'IS IT OK TODAY?' });
